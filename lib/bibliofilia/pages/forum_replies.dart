ddff51057d3727d235467bc4368a7f4ded646a79
pebintk <pendar.kasdiono@gmail.com>
2023-12-11 16:02:59 +0700

    Added ForumRepliesHead

--- lib/bibliofilia/pages/forum_replies.dart
+++ lib/bibliofilia/pages/forum_replies.dart
@@ -16,55 +16,72 @@ class ForumRepliesPage extends StatefulWidget {
 
 class _ForumRepliesPageState extends State<ForumRepliesPage> {
   late Future<List<ForumReplies>> replies;
+  late Future<List<Forum>> head;
 
   @override
   void initState() {
     super.initState();
     replies = fetchReplies();
+    head = fetchRepliesHead();
   }
 
   void deleteReply(int replyId) async {
-  var url = Uri.parse('http://localhost:8000/bibliofilia/delete_replies_flutter/');
+    var url =
+        Uri.parse('http://localhost:8000/bibliofilia/delete_replies_flutter/');
 
-  // Assuming you have a way to get the current username. Replace with actual username.
-  String currentUsername = "currentUser"; // Replace with actual username logic
+    // Assuming you have a way to get the current username. Replace with actual username.
+    String currentUsername =
+        "currentUser"; // Replace with actual username logic
 
-  var response = await http.delete(
-    url,
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, dynamic>{
-      'reply_id': replyId,
-      'username': currentUsername,
-    }),
-  );
-
-  if (response.statusCode == 200) {
-    // Handle successful deletion
-    setState(() {
-      replies = fetchReplies(); // Refresh the replies list after deletion
-    });
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Reply deleted successfully')),
+    var response = await http.delete(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'reply_id': replyId,
+        'username': currentUsername,
+      }),
     );
-  } else {
-    // Handle error
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Failed to delete reply: ${response.body}')),
-    );
+
+    if (response.statusCode == 204  ) {
+      // Handle successful deletion
+      setState(() {
+        replies = fetchReplies(); // Refresh the replies list after deletion
+        head = fetchRepliesHead();
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Reply deleted successfully')),
+      );
+    } else {
+      // Handle error
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to delete reply: ${response.body}')),
+      );
+    }
   }
-}
-
 
   Future<List<ForumReplies>> fetchReplies() async {
-    var url = Uri.parse('http://localhost:8000/bibliofilia/get_ForumReply_flutter/${widget.forumId}/');
+    var url = Uri.parse(
+        'http://localhost:8000/bibliofilia/get_ForumReply_flutter/${widget.forumId}/');
     var response = await http.get(url);
 
     if (response.statusCode == 200) {
       return forumRepliesFromJson(response.body);
     } else {
       throw Exception('Failed to load replies');
+    }
+  }
+
+  Future<List<Forum>> fetchRepliesHead() async {
+    var url = Uri.parse(
+        'http://localhost:8000/bibliofilia/get_ForumReplyHead_json_flutter/${widget.forumId}/');
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return forumFromJson(response.body);
+    } else {
+      throw Exception('Failed to load head');
     }
   }
 
@@ -78,15 +95,53 @@ class _ForumRepliesPageState extends State<ForumRepliesPage> {
       ),
       body: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 10.0),
-            child: Text('Feature',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          FutureBuilder<List<Forum>>(
+            future: head,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text('Error: ${snapshot.error}',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                );
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text('Forum details not available.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+                );
+              } else {
+                // Assuming the first item is the forum head
+                Forum forumHead = snapshot.data!.first;
+                return Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 10.0),
+                      child: Text(
+                        forumHead.fields.bookName,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Text(
+                      forumHead.fields.userReview,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                );
+              }
+            },
           ),
           const SizedBox(height: 10),
           Expanded(
-            // Use Expanded here
             child: buildForumRepliesList(),
           ),
         ],
@@ -94,18 +149,7 @@ class _ForumRepliesPageState extends State<ForumRepliesPage> {
     );
   }
 
-  Widget buildForumDetails(Forum forumDetails) {
-  return Container(
-    // styling
-    child: Column(
-      children: [
-        Text(forumDetails.fields.userReview, /* styling */),
-        Text(forumDetails.fields.bookName, /* styling */),
-        // other details as needed
-      ],
-    ),
-  );
-}
+
 
   Widget buildForumRepliesList() {
     return FutureBuilder<List<ForumReplies>>(
@@ -123,6 +167,7 @@ class _ForumRepliesPageState extends State<ForumRepliesPage> {
             itemBuilder: (context, index) {
               ForumReplies reply = snapshot.data![index];
               RepliesFields fields = reply.fields;
+            
 
               return Container(
                 margin: const EdgeInsets.symmetric(horizontal: 16),
@@ -138,13 +183,13 @@ class _ForumRepliesPageState extends State<ForumRepliesPage> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              // Text(
-                              //   fields.forum.toString(),
-                              //   style: const TextStyle(
-                              //       color: Colors.white,
-                              //       fontWeight: FontWeight.bold,
-                              //       fontSize: 20),
-                              // ),
+                              Text(
+                                fields.forum.toString(),
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20),
+                              ),
                               Text(
                                 fields.username, // Displaying the username
                                 style: const TextStyle(color: Colors.white),
@@ -158,7 +203,11 @@ class _ForumRepliesPageState extends State<ForumRepliesPage> {
                                 style: const TextStyle(color: Colors.white),
                               ),
                               TextButton(
-                                onPressed: () => deleteReply(reply.pk), // Call delete function
+                                onPressed:() async  {
+                                  setState(() {
+                                    deleteReply(reply.pk);  
+                                  });
+                                }, // Call delete function
                                 child: const Text(
                                   'Delete',
                                   style: TextStyle(color: Colors.red),
